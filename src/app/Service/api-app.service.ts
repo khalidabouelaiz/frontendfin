56b41dec3266435ee9613d8d229c711f719b8255
khalidabouelaiz <141508141+khalidabouelaiz@users.noreply.github.com>
2023-10-30 11:31:27 +0100

    Update api-app.service.ts

--- src/app/Service/api-app.service.ts
+++ src/app/Service/api-app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
 import { Observable } from 'rxjs';
 
-const baseUrl = 'http://34.107.20.179/';
+const baseUrl = 'http://34.107.20.179:7777';
 const baseUr = 'http://34.107.20.179/users';
 const url = 'http://34.107.20.179/getLotByIdUser';
 
