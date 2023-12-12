9a8a9345a89c72958bd436f3c0de9d61b0786d84
khalidabouelaiz <141508141+khalidabouelaiz@users.noreply.github.com>
2023-12-12 19:00:56 +0100

    Update about-us.component.ts

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
 import { RouteConfigLoadEnd } from '@angular/router';
 import { Router } from '@angular/router';
 
@@ -8,8 +8,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./about-us.component.css'],
 })
 export class AboutUsComponent implements OnInit {
-  constructor(public router: Router) {}
-
+  constructor(public router: Router, private el: ElementRef) {}
+  @HostListener('document:click', ['$event'])
+  closeNavbarOnOutsideClick(event: Event): void {
+    const navbar = this.el.nativeElement.querySelector('.navbar-collapse.show');
+    if (navbar && !navbar.contains(event.target as Node)) {
+      navbar.classList.remove('show');
+    }
+  }
   ngOnInit(): void {}
   onLogin() {
     this.router.navigate(['/login']);
