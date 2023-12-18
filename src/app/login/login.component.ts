6019bcb5fa2722a6a7411f2c7c1124ff011d3d77
khalidabouelaiz <141508141+khalidabouelaiz@users.noreply.github.com>
2023-12-18 15:02:35 +0100

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -52,7 +52,7 @@ export class LoginComponent implements OnInit {
   helper = new JwtHelperService();
   loginUserForm: FormGroup;
   user: SocialUser;
-  admin = [{ email1: 'administrateur@hotmail.com', password1: 'Driss123' }];
+  admin = [{ email1: 'administrateur@hotmail.com', password1: 'Admin123' }];
   employerac = [{ email2: 'employer@hotmail.com', password2: '12345678' }];
 
   constructor(
